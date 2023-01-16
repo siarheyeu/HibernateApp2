35bf0e69d444c271ff23a73e02e07ec203002a14
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-01-16 22:40:17 +0300

    Relation Many-To-Many in Hibernate - list of actors for the film

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -27,18 +27,9 @@ public class App
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Movie movie = new Movie("Pulp fiction", 1994);
-            Actor actor1 = new Actor("Harvey Keitel", 81);
-            Actor actor2 = new Actor("Samuel L. Jackson", 72);
+            Movie movie = session.get(Movie.class, 1);
+            System.out.println(movie.getActors());
 
-            movie.setActors((new ArrayList<>(List.of(actor1, actor2))));
-
-            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-
-            session.save(movie);
-            session.save(actor1);
-            session.save(actor2);
 
             session.getTransaction().commit();
 
